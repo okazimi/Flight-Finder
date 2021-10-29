58e9271ab5bd206e06ca3394ed597cc9e49e71b3
Omar Kazimi <55765190+okazimi@users.noreply.github.com>
2021-10-29 09:45:29 +0200

    Update notification_manager.py

--- notification_manager.py
+++ notification_manager.py
@@ -5,23 +5,23 @@ import smtplib
 class NotificationManager:
 
     def __init__(self):
-        self.account_sid = "ACe0b4deacc51b2b7e260de3cdba32a2df"
-        self.auth_token = "3f6e7cf85a054c0f1c293fbd195a772d"
+        self.account_sid = MY_ACCT_SID
+        self.auth_token = MY_AUTH_TOKEN
         self.client = Client(self.account_sid, self.auth_token)
 
     def send_text(self, message):
         message = self.client.messages \
                         .create(
                              body=message,
-                             from_='+18482891475',
-                             to='+15708142353'
+                             from_='+111111111',
+                             to='+1111111111'
                          )
 
         print(message.sid)
 
     def send_emails(self, recipient_email, msg):
-        USERNAME = "pythoncreator@hotmail.com"
-        PASSWORD = "THECHOSENONE1"
+        USERNAME = "EMAIL@EMAIL.COM"
+        PASSWORD = "PASSWORD"
         smtp = smtplib.SMTP("smtp.live.com")
         smtp.starttls()
         smtp.login(USERNAME, PASSWORD)
